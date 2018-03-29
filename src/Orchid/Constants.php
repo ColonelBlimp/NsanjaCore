dd045d019e20f84a677b5abdfe3b26c2ca6f540c
ColonelBlimp <ColonelBlimp@users.noreply.github.com>
2018-03-29 17:58:18 +0200

    Minor update

--- src/Orchid/Constants.php
+++ src/Orchid/Constants.php
@@ -2,9 +2,21 @@
 
 namespace Orchid;
 
+/**
+ * This interface defines the constants used by the Orchid platform. The reason why this is used rather than
+ * <code>define</code> within a file is that this interface can be namespaced!
+ * @author Marc L. Veary
+ * @namespace Orchid
+ */
 interface Constants
 {
+    /**
+     * A shortened version of DIRECTORY_SEPARATOR
+     */
     public const DS = DIRECTORY_SEPARATOR;
 
-    public const PHP_FILE_PLATFORMCONFIG = 'PlatformConfig.php';
+    /**
+     * The filename for the default platform configuration file (PlatformConfig.php)
+     */
+    public const FILE_PLATFORM_CONFIG_PHP = 'PlatformConfig.php';
 }
