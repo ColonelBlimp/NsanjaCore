32c7e8fdb10a79d0b911603255ee4a5e8d08f9ab
ColonelBlimp <ColonelBlimp@users.noreply.github.com>
2018-03-29 18:01:36 +0200

    Update

--- src/Orchid/Constants.php
+++ src/Orchid/Constants.php
@@ -16,7 +16,14 @@ interface Constants
     public const DS = DIRECTORY_SEPARATOR;
 
     /**
-     * The filename for the default platform configuration file (PlatformConfig.php)
+     * The filename for the default platform configuration file (PlatformConfig.php). The default location for this
+     * file is in the [app_root]/config directory.
      */
     public const FILE_PLATFORM_CONFIG_PHP = 'PlatformConfig.php';
+
+    /**
+     * The filename for the default base platform configuration file (Config.php). The default location for this
+     * file is in the same directory as the Configuration implementation classfile.
+     */
+    public const FILE_BASE_CONFIG_PHP = 'Config.php';
 }
